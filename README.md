6e224ef306f0d5c16d05df3b8db9b6745e20d652
Diogo Alves <diogo.alves.bcl@gmail.com>
2015-05-01 19:13:43 +0100

    Read multiple yaml files with zip codes of different countries

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Or install it yourself as:
 ## Usage
 
 ```ruby
-ZipCodes.identify('30301')
+ZipCodes.identify('US', 30301')
 # => {:state_code=>"GA", :state_name=>"Georgia", :city=>"Atlanta", :time_zone=>"America/New_York"}
 # First run will take a while, as the yaml has to be loaded
 ```
--- lib/zip-codes.rb
+++ lib/zip-codes.rb
@@ -4,15 +4,21 @@ module ZipCodes
   VERSION = '0.2.0'
 
   class << self
-    def identify code
-      db[code]
+    def identify country, code
+      db[country.upcase][code]
     end
 
     def db
       @db ||= begin
         this_file = File.expand_path(File.dirname(__FILE__))
-        us_data = File.join(this_file, "data", "US.yml")
-        YAML.load(File.open(us_data))
+        data_files_path = Dir[File.join(this_file, "data", "*.yml")]
+
+        zip_codes_by_country = {}
+        data_files_path.each do |path|
+          name = File.basename path, ".yml"
+          zip_codes_by_country[name.upcase] = YAML.load(File.open(path))
+        end
+        zip_codes_by_country
       end
     end
 
